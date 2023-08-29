713c6a221618d7c781ef7f7955350983728c33c6
Anthony D <anthonydantas15@gmail.com>
2023-08-29 15:36:54 -0400

    :sparkles: feat: description screen

---
+++ src/pages/Description/Description.js
@@ -0,0 +1,61 @@
+import React from "react";
+
+import Navbar from "../../components/Navbar";
+
+import "./style.css";
+
+import { FaStar } from "react-icons/fa6";
+
+export default function Description() {
+  return (
+    <div>
+      <Navbar />
+      <div className="container">
+        <h2>Pousada Mar da Luz</h2>
+        <div className="content">
+          <div className="image">
+            <img src="hotel.svg"></img>
+            <div className="valores">
+              <div className="valueDescription">
+                <h6>Valor da Diária:</h6>
+                <h6>Custo fixo:</h6>
+                <h6>Total:</h6>
+              </div>
+              <div className="price">
+                <h6>R$529,80</h6>
+                <h6>R$50,00</h6>
+                <h6>R$579,80</h6>
+              </div>
+            </div>
+          </div>
+          <div className="descriptionOfHotel">
+            <p>
+              Bem-vindo ao refúgio sereno e encantador que é a Pousada Mar da
+              Luz! Localizada em um cenário deslumbrante à beira-mar, nossa
+              pousada é um verdadeiro oásis de tranquilidade e beleza natural.
+              Com vistas deslumbrantes do oceano que se estendem até onde os
+              olhos podem ver, oferecemos uma experiência única de relaxamento e
+              rejuvenescimento. A Pousada Mar da Luz é um retiro acolhedor que
+              combina conforto moderno com a autenticidade de uma atmosfera
+              beira-mar. Nossos quartos elegantemente decorados são projetados
+              para proporcionar uma estadia aconchegante e memorável. Cada
+              detalhe foi cuidadosamente considerado para garantir que nossos
+              hóspedes se sintam em casa enquanto desfrutam da brisa do mar e
+              dos sons suaves das ondas.
+            </p>
+            <div className="star">
+              <FaStar size={40} color="yellow" />
+              <FaStar size={40} color="yellow" />
+              <FaStar size={40} color="yellow" />
+              <FaStar size={40} color="yellow" />
+              <FaStar size={40} color="yellow" />
+            </div>
+            <div className="buttonAcomodacao">
+              <button className="touchButton">Ver Acomodação</button>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
--- src/routes.js
+++ src/routes.js
@@ -1,31 +1,26 @@
-import React from 'react';
-import { BrowserRouter as Router, Route, Routes, Navigate } from 'react-router-dom';
+import React from "react";
+import {
+  BrowserRouter as Router,
+  Route,
+  Routes,
+  Navigate,
+} from "react-router-dom";
 
-import Login from './pages/Login';
-import SignUp from './pages/Register';
-import Home from './pages/Home';
-import Error from './pages/Error';
+import Login from "./pages/Login";
+import SignUp from "./pages/Register";
+import Home from "./pages/Home";
+import Error from "./pages/Error";
+import Description from "./pages/Description/Description";
 
 export default function Rotas() {
   return (
     <Router>
       <Routes>
-        <Route
-          path="/"
-          element={<SignUp />}
-        ></Route>
-        <Route
-          path="/login"
-          element={<Login />}
-        ></Route>
-        <Route
-          path="/home"
-          element={<Home />}
-        ></Route>
-        <Route
-          path="*"
-          element={<Error />}
-        ></Route>
+        <Route path="/" element={<SignUp />}></Route>
+        <Route path="/login" element={<Login />}></Route>
+        <Route path="/home" element={<Home />}></Route>
+        <Route path="*" element={<Error />}></Route>
+        <Route path="/description" element={<Description />}></Route>
       </Routes>
     </Router>
   );
