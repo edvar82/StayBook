2132f4c40658a4b7dcf8e47a42eb99751660c161
davisouzal <dsl@ic.ufal.br>
2023-10-23 09:49:56 -0300

    :sparkles: feat: payment done

--- src/pages/Payment/Payment.js
+++ src/pages/Payment/Payment.js
@@ -11,10 +11,11 @@ import {
   MDBInput,
   MDBBtn,
 } from 'mdb-react-ui-kit';
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 
 import Navbar from '../../components/Navbar';
-import luxo from '../../assets/img/luxo.jpg';
+
+// const [cartao, setCartao] = useState([]);
 
 function calcularDiferencaDias(diaIn, mesIn, anoIn, diaOut, mesOut, anoOut) {
   const dataIn = new Date(anoIn, mesIn - 1, diaIn);
@@ -27,6 +28,45 @@ function calcularDiferencaDias(diaIn, mesIn, anoIn, diaOut, mesOut, anoOut) {
   return diferencaEmDias;
 }
 
+function formatDateTime(dia, mes, ano, condicao) {
+  // Verifique se os parâmetros são válidos
+  if (dia < 1 || dia > 31 || mes < 1 || mes > 12 || ano < 1900) {
+    return null; // Retorne null se os parâmetros forem inválidos
+  }
+
+  // Crie um objeto de data
+  const data = new Date(ano, mes - 1, dia);
+
+  if (condicao === 'IN') {
+    // Se a condição for 'IN', defina a hora para 14:00 (2:00 PM)
+    data.setHours(14, 0, 0, 0);
+  } else if (condicao === 'OUT') {
+    // Se a condição for 'OUT', defina a hora para 12:00 (12:00 PM)
+    data.setHours(12, 0, 0, 0);
+  } else {
+    return null; // Retorna null se a condição for inválida
+  }
+
+  return data;
+}
+
+// async function getCartaoFromCliente(clienteId) {
+//   const response = await axios.get(
+//     `http://localhost:3001/cartao/${clienteId}`
+//   );
+  
+//     console.log(response.data);
+
+//   setCartao(response.data);
+// }
+
+// useEffect(() => {
+//   const clienteId = localStorage.getItem("clienteId");
+//   getCartaoFromCliente(clienteId);
+// }, []);
+
+
+
 const diaIn = localStorage.getItem("diaIn");
 const mesIn = localStorage.getItem("mesIn");
 const anoIn = localStorage.getItem("anoIn");
@@ -38,6 +78,30 @@ const dias = calcularDiferencaDias(diaIn, mesIn, anoIn, diaOut, mesOut, anoOut);
 const value = 120.45;
 const img = localStorage.getItem("imagem");
 const nome = localStorage.getItem("nome");
+const dataIn = formatDateTime(diaIn, mesIn, anoIn, 'IN');
+const dataOut = formatDateTime(diaOut, mesOut, anoOut, 'OUT');
+
+async function payment(){
+  try{
+    const response = await axios.post(`http://localhost:3001/cartao/payment`, {
+    clientId: localStorage.getItem("clienteId"),
+    hotelId: 1, // aqui coloca o id do hotel
+    nomeHotel: nome,
+    checkIn: dataIn,
+    checkOut: dataOut,
+    numQuartos: 1,
+    valor: value * dias,
+    metodoPagamento: "Cartão de Crédito",
+    idCartao: 1, // aqui coloca o id do cartão
+  });
+  if(response.data){
+    alert("Pagamento realizado com sucesso");
+    window.location.href = "/home";
+  }
+  } catch {
+    console.log("Erro ao realizar pagamento");
+  }
+}
 
 function PaymentPage() {
   return (
@@ -55,6 +119,33 @@ function PaymentPage() {
                       <b>1. Cartão</b>
                     </p>
                   </div>
+                </div>
+                <div className='pt-3'>
+                {/* {cartao ? (
+                  cartao.map((cartao) => (
+                    <div className="d-flex flex-row pb-3">
+                    <div className="rounded border border-black border-2 d-flex w-100 p-3 align-items-center">
+                      <div className="d-flex align-items-center pe-3">
+                        <MDBRadio name="radioDebit" id="radioDebit" />
+                      </div>
+                      <MDBIcon
+                        fab
+                        icon="cc-mastercard"
+                        size="3x"
+                        className="text-black pe-3"
+                      />
+                      <div className="d-flex flex-column">
+                        <p className="mb-1 small text-black">
+                          Cartão de Débito
+                        </p>
+                        <h6 className="mb-0 text-black">{cartao.numero}</h6>
+                      </div>
+                    </div>
+                  </div>
+                  ))
+                ) : (
+                  <div>Nenhum cartão</div>
+                )} */}
                 </div>
                 <div className="pt-3">
                   <div className="d-flex flex-row pb-3">
@@ -152,7 +243,7 @@ function PaymentPage() {
                   <a href="#!" className="text-muted">
                     Voltar
                   </a>
-                  <MDBBtn size="lg" style={{ backgroundColor: 'black' }}>
+                  <MDBBtn size="lg" style={{ backgroundColor: 'black' }} onClick="">
                     Usar esta forma de pagamento
                   </MDBBtn>
                 </div>
@@ -162,7 +253,7 @@ function PaymentPage() {
           <MDBCol md="4">
             <h5 className="mb-4">Resumo</h5>
             <MDBCard className="text-black">
-              <MDBCardImage src={img} position="top" alt="Apple Computer" />
+              <MDBCardImage src={img} position="top" alt={nome ? nome : "Nome do hotel"} />
               <MDBCardBody>
                 <div className="text-center">
                   <MDBCardTitle>{nome}</MDBCardTitle>
